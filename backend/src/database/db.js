537b31e31734cef9863544d217c4d0645b40903f
Derley Lopez <derley1911@gmail.com>
2025-08-03 17:10:52 -0500

    CAMBIOS

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 
 
 const mysql = require('mysql2/promise');
+
 console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_NAME:', process.env.DB_NAME);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
