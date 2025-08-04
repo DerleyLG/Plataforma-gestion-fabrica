9a6b3cde23513a0f335300a7c6b7dee236a6d6e6
Derley Lopez <derley1911@gmail.com>
2025-08-04 17:11:16 -0500

    Correcion en archivo nginx.conf

--- backend/app.js
+++ backend/app.js
@@ -44,8 +44,7 @@ const compraMateriaPrimaRoutes = require('./src/routes/compraMateriaPrimaRoutes.
 const app = express();
 app.use(cors(
   {
-      origin: ['http://abakosoft-frontend-bef8en-1dc861-148-230-91-63.traefik.me'], 
-  credentials: true
+       origin: '*'
   }
 ));
 app.use(express.json());
