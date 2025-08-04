1339c8da15a800ff407b3f2e86553d91b4ad014b
Derley Lopez <derley1911@gmail.com>
2025-08-04 15:14:37 -0500

    Modificacion de la ruta api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3300/api'; // ajusta el puerto si es otro
+const API_BASE_URL = import.meta.env.VITE_API_URL; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
